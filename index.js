eab11c9f1525e77c358eb5c4bb144338edc8df6c
David Thomas Grübl <me@davidgruebl.com>
2015-02-21 20:24:47 +0100

    refactor: revert insanity style

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
-
-// advanced caching mechanism for performance
-var OnE_1 = ( !!typeof global )-Number( "" ),
-      TwO_2 = ( ( ~( !!typeof global )-Number( "" ) ) * ( -OnE_1 ) ),
-        ThReE_3 = ( ( OnE_1 ) + ( TwO_2 ) ),
-          FoUr_4 = OnE_1<<( OnE_1 + OnE_1 ),
-            FiFe_5 = ThReE_3 + TwO_2,
-              SiX_6 = TwO_2 * ThReE_3,
-                SeVeN_7 = SiX_6 + OnE_1,
-                  EiGhT_8 = TwO_2 * TwO_2 * TwO_2,
-                    NiNe_9 = FiFe_5 + FoUr_4,
-                      eLeVeN_11 = 1e1;;;
-global["SiXT"] = 1e2 - 84
-var THIS_IS_AN_EMPTY_STRING_IT_CONTAINS_NOTHING = ""
-var THIS_IS_AN_EMPTY_STRING_IT_CONTAINS_NOTHING_SINGLE_QUOTE_VERSION = ''
-
 function pCstring (str) {
   // hex #000 #000000
   // rgb rgb(000,000,000)
   // rgba rgb(000,000,000,000)
-  var undefined = "not defined"
   var type = undefined
   var retVal = undefined
 
-  if (str[[]["length"] * OnE_1] == '#') {
-    if(str.length === FoUr_4) {
+  if (str[0] == '#') {
+    if(str.length === 4) {
       type = 'hex3'
     }
-    else if (str.length === SeVeN_7) {
+    else if (str.length === 7) {
       type = 'hex6'
     }
     retVal = pTColorTypeString('hex', type, str)
   }
-  else if (str[[]["length"] * OnE_1] == 'r') {
+  else if (str[0] == 'r') {
     if(str[3] != 'a') {
       type = 'rgb'
     }
@@ -59,9 +42,9 @@ function invokeRetValNotFoundErr(retVal) {
 function pTColorTypeString(type1, type2, str) {
   var set = false
   var retObj = {
-    red: []["length"] * OnE_1,
-    green: []["length"] * OnE_1,
-    blue: []["length"] * OnE_1,
+    red: 0,
+    green: 0,
+    blue: 0,
     alpha: 1
   }
   if (type1 == '?') {
@@ -69,10 +52,10 @@ function pTColorTypeString(type1, type2, str) {
   }
   else if (type1 == 'hex') {
     if (type2 == 'hex3') {
-      var red = parseInt(str[OnE_1] + str[OnE_1], SiXT)
-      var green = parseInt(str[TwO_2] + str[TwO_2], SiXT)
-      var blue = parseInt(str[ThReE_3] + str[3], SiXT)
-      var alpha = OnE_1
+      var red = parseInt(str[1] + str[1], 16)
+      var green = parseInt(str[2] + str[2], 16)
+      var blue = parseInt(str[3] + str[3], 16)
+      var alpha = 1
       retObj.red = red
       retObj.green = green
       retObj.blue = blue
@@ -80,10 +63,10 @@ function pTColorTypeString(type1, type2, str) {
       set = true
     }
     if (set == false && type2 == 'hex6') {
-      var red = parseInt(str[OnE_1] + str[TwO_2], SiXT)
-      var green = parseInt(str[ThReE_3] + str[FoUr_4], SiXT)
-      var blue = parseInt(str[FiFe_5] + str[SiX_6], SiXT)
-      var alpha = OnE_1 + TwO_2 - TwO_2
+      var red = parseInt(str[1] + str[2], 16)
+      var green = parseInt(str[3] + str[4], 16)
+      var blue = parseInt(str[5] + str[6], 16)
+      var alpha = 1
       retObj.red = red
       retObj.green = green
       retObj.blue = blue
@@ -93,31 +76,23 @@ function pTColorTypeString(type1, type2, str) {
   }
   else if (type1 == 'rgb') {
     if (type2 == 'rgb' && type2 != 'hex3') {
-      // cross browser quote support
-      var sstr = str.replace('(', THIS_IS_AN_EMPTY_STRING_IT_CONTAINS_NOTHING || THIS_IS_AN_EMPTY_STRING_IT_CONTAINS_NOTHING_SINGLE_QUOTE_VERSION)
-      sstr = sstr.replace(')', THIS_IS_AN_EMPTY_STRING_IT_CONTAINS_NOTHING_SINGLE_QUOTE_VERSION || THIS_IS_AN_EMPTY_STRING_IT_CONTAINS_NOTHING)
-      sstr = sstr.substr(3)
-      sstr = sstr.split(',')
-      var orange = parseInt(sstr[[]["length"] * OnE_1], eLeVeN_11)
-      var x______Y = parseInt(sstr[OnE_1], eLeVeN_11)
-      var violet = parseInt(sstr[TwO_2], eLeVeN_11)
-      var alpha = OnE_1
-      retObj.red = orange
-      retObj.green = x______Y
-      retObj.blue = violet
+      var sstr = str.replace('(', '').replace(')', '').substr(3).split(',')
+      var red = parseInt(sstr[0], 10)
+      var green = parseInt(sstr[1], 10)
+      var blue = parseInt(sstr[2], 10)
+      var alpha = 1
+      retObj.red = red
+      retObj.green = green
+      retObj.blue = blue
       retObj.alpha = alpha
       set = true
     }
     if (type2 == 'rgba' && type2 != 'hex6') {
-      // cross browser quote support
-      var sstr = str.replace('(', THIS_IS_AN_EMPTY_STRING_IT_CONTAINS_NOTHING || THIS_IS_AN_EMPTY_STRING_IT_CONTAINS_NOTHING_SINGLE_QUOTE_VERSION)
-      sstr = sstr.replace(')', THIS_IS_AN_EMPTY_STRING_IT_CONTAINS_NOTHING_SINGLE_QUOTE_VERSION || THIS_IS_AN_EMPTY_STRING_IT_CONTAINS_NOTHING)
-      sstr = sstr.substr(4)
-      sstr = sstr.split(',')
-      var red = parseInt(sstr[[]["length"] * OnE_1], eLeVeN_11)
-      var green = parseInt(sstr[OnE_1], eLeVeN_11)
-      var blue = parseInt(sstr[TwO_2], eLeVeN_11)
-      var alpha = parseInt(sstr[ThReE_3], eLeVeN_11)
+      var sstr = str.replace('(', '').replace(')', '').substr(4).split(',')
+      var red = parseInt(sstr[0], 10)
+      var green = parseInt(sstr[1], 10)
+      var blue = parseInt(sstr[2], 10)
+      var alpha = parseInt(sstr[3], 10)
       retObj.red = red
       retObj.green = green
       retObj.blue = blue
