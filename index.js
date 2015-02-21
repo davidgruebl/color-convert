d47cc82a7b341c30d59dea19a0f3d779b63ada8b
David Thomas Grübl <me@davidgruebl.com>
2015-02-21 21:24:18 +0100

    refactor: remove use of retObj

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
 module.exports = function (str) {
-  var retObj = {
-    red: 0,
-    green: 0,
-    blue: 0,
-    alpha: 1
-  }
-
   if (str[0] === '#' && str.length === 4) {
-    retObj.red = parseInt(str[1] + str[1], 16)
-    retObj.green = parseInt(str[2] + str[2], 16)
-    retObj.blue = parseInt(str[3] + str[3], 16)
-    return retObj
+    return {
+      red: parseInt(str[1] + str[1], 16),
+      green: parseInt(str[2] + str[2], 16),
+      blue: parseInt(str[3] + str[3], 16),
+      alpha: 1
+    }
   }
-
   if (str[0] === '#' && str.length === 7) {
-    retObj.red = parseInt(str[1] + str[2], 16)
-    retObj.green = parseInt(str[3] + str[4], 16)
-    retObj.blue = parseInt(str[5] + str[6], 16)
-    return retObj
+    return {
+      red: parseInt(str[1] + str[2], 16),
+      green: parseInt(str[3] + str[4], 16),
+      blue: parseInt(str[5] + str[6], 16),
+      alpha: 1
+    }
   }
-
   if (str[0] === 'r' && str[3] !== 'a') {
     var sstr = str.replace('(', '').replace(')', '').substr(3).split(',')
-    retObj.red = parseInt(sstr[0], 10)
-    retObj.green = parseInt(sstr[1], 10)
-    retObj.blue = parseInt(sstr[2], 10)
-    return retObj
+    return {
+      red: parseInt(sstr[0], 10),
+      green: parseInt(sstr[1], 10),
+      blue: parseInt(sstr[2], 10),
+      alpha: 1
+    }
   }
-
   if (str[0] === 'r' && str[3] === 'a') {
     var sstr = str.replace('(', '').replace(')', '').substr(4).split(',')
-    retObj.red = parseInt(sstr[0], 10)
-    retObj.green = parseInt(sstr[1], 10)
-    retObj.blue = parseInt(sstr[2], 10)
-    retObj.alpha = parseInt(sstr[3], 10)
-    return retObj
+    return {
+      red: parseInt(sstr[0], 10),
+      green: parseInt(sstr[1], 10),
+      blue: parseInt(sstr[2], 10),
+      alpha: parseInt(sstr[3], 10)
+    }
   }
-
   throw new Error('Invalid color string')
 }
