94ff41dc6d323995e2c2fe4073c7fb5bfc3a2cc8
David Thomas Grübl <me@davidgruebl.com>
2015-02-21 20:47:37 +0100

    refactor: store values directly

--- index.js
+++ index.js
@@ -35,46 +35,30 @@ function pTColorTypeString(type, str) {
   }
 
   if (type === 'hex3') {
-    var red = parseInt(str[1] + str[1], 16)
-    var green = parseInt(str[2] + str[2], 16)
-    var blue = parseInt(str[3] + str[3], 16)
-    var alpha = 1
-    retObj.red = red
-    retObj.green = green
-    retObj.blue = blue
-    retObj.alpha = alpha
+    retObj.red = parseInt(str[1] + str[1], 16)
+    retObj.green = parseInt(str[2] + str[2], 16)
+    retObj.blue = parseInt(str[3] + str[3], 16)
   }
+
   if (type === 'hex6') {
-    var red = parseInt(str[1] + str[2], 16)
-    var green = parseInt(str[3] + str[4], 16)
-    var blue = parseInt(str[5] + str[6], 16)
-    var alpha = 1
-    retObj.red = red
-    retObj.green = green
-    retObj.blue = blue
-    retObj.alpha = alpha
+    retObj.red = parseInt(str[1] + str[2], 16)
+    retObj.green = parseInt(str[3] + str[4], 16)
+    retObj.blue = parseInt(str[5] + str[6], 16)
   }
+
   if (type === 'rgb') {
     var sstr = str.replace('(', '').replace(')', '').substr(3).split(',')
-    var red = parseInt(sstr[0], 10)
-    var green = parseInt(sstr[1], 10)
-    var blue = parseInt(sstr[2], 10)
-    var alpha = 1
-    retObj.red = red
-    retObj.green = green
-    retObj.blue = blue
-    retObj.alpha = alpha
+    retObj.red = parseInt(sstr[0], 10)
+    retObj.green = parseInt(sstr[1], 10)
+    retObj.blue = parseInt(sstr[2], 10)
   }
+
   if (type === 'rgba') {
     var sstr = str.replace('(', '').replace(')', '').substr(4).split(',')
-    var red = parseInt(sstr[0], 10)
-    var green = parseInt(sstr[1], 10)
-    var blue = parseInt(sstr[2], 10)
-    var alpha = parseInt(sstr[3], 10)
-    retObj.red = red
-    retObj.green = green
-    retObj.blue = blue
-    retObj.alpha = alpha
+    retObj.red = parseInt(sstr[0], 10)
+    retObj.green = parseInt(sstr[1], 10)
+    retObj.blue = parseInt(sstr[2], 10)
+    retObj.alpha = parseInt(sstr[3], 10)
   }
   return retObj
 }
